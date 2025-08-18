410e62f83aa784afeafc0fcb59cac5e050383f0c
jayanthi m <jayanthi.m@ellantec.com>
2025-08-18 15:26:50 +0530

    demo-prediction-file-added

---
+++ assign.py
@@ -0,0 +1,25 @@
+# num1=int(input("Enter a number 1 : "))
+# num2=int(input("Enter a number 2"))
+# print(num1)
+# num1-=5
+# print(num1)
+# num1+=10
+# print(num1)
+# num1+=20
+# print(num1)
+
+# num1=num1+num2
+# num1+=num2
+# print("And: ",num1&num2)
+# print("OR : ",num1|num2)
+# print("NOT :",~num1)
+# print("XOR :",num1^num2)   
+
+
+# a=10
+# b=10
+# print(a is not b)
+
+a=[23,56,78,12,67]
+print(293 in a)
+print(293 not in a)
